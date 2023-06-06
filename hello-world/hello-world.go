81c9a2d13a19b881203f370dddb9d1c10dadd9d9
Ramanujam Gond <ramanujamgond@gmail.com>
2023-06-06 21:20:13 +0530

    gone through go basic syntax, data types, varaibles and print a hello world program

---
+++ hello-world/hello-world.go
@@ -0,0 +1,8 @@
+// legendary Hello World in golang
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World!")
+}
---
+++ variables/variables.go
@@ -0,0 +1,20 @@
+// variables in go
+
+package main
+
+import "fmt"
+
+func main() {
+	var a = "Initial value"
+	fmt.Println(a)
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+	var d = true
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+
+	f := "apple"
+	fmt.Println(f)
+}
